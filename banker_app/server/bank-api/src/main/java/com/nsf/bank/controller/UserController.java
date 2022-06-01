acc3c6ab62ddee835be852cb333fe0fde4c524c4
Aliseth <charlotte.saidi@outlook.fr>
2022-06-01 11:19:45 +0200

    fix : added CrossOrigin annotations in controllers

--- banker_app/server/bank-api/src/main/java/com/nsf/bank/controller/UserController.java
+++ banker_app/server/bank-api/src/main/java/com/nsf/bank/controller/UserController.java
@@ -11,6 +11,7 @@ import com.nsf.bank.service.JWTUserService;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/users")
 public class UserController {
 	
