a807600c25a09c3c5e6672362485c7c1eb2da3f7
QVan27 <qvannarath@gmail.com>
2022-06-20 10:58:05 +0200

    ✨ Add Account Layout Component & Add Services

--- banker_app/client/bank-app/src/layouts/Auth.js
+++ banker_app/client/bank-app/src/layouts/Auth.js
@@ -1,26 +1,25 @@
 import React, { useEffect } from "react";
-import { useRouter } from 'next/router';
-import { userService } from '@services/index';
+import { useRouter } from "next/router";
+import { userService } from "@services/index";
 
 // components
 
 import Navbar from "@components/Navbars/AuthNavbar.js";
 
-
 export default function Auth({ children }) {
-    const router = useRouter();
+  const router = useRouter();
 
-    useEffect(() => {
-        // redirect to home if already logged in
-        if (userService.userValue) {
-            router.push('/');
-        }
+  useEffect(() => {
+    // redirect to home if already logged in
+    if (userService.userValue) {
+      router.push("/admin/dashboard");
+    }
 
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    return (
-      <>
+  return (
+    <>
       <Navbar transparent />
       <main>
         <section className="relative w-full h-full py-40 min-h-screen">
@@ -31,9 +30,8 @@ export default function Auth({ children }) {
             }}
           ></div>
           {children}
-          {/* <FooterSmall absolute /> */}
         </section>
       </main>
     </>
-    );
+  );
 }
--- banker_app/client/bank-app/src/services/alert.services.js
+++ banker_app/client/bank-app/src/services/alert.services.js
@@ -1,4 +1,4 @@
-import { Subject } from "rxjs";
+import { Subject } from 'rxjs';
 import { filter } from "rxjs/operators";
 
 export const alertService = {
