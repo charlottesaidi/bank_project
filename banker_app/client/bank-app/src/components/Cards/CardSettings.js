1f22e735c1dbd9d114c3f8db1927d61cb47b4ce0
QVan27 <qvannarath@gmail.com>
2022-06-24 11:16:50 +0200

    🐛 Fix Data Settings

--- banker_app/client/bank-app/src/components/Cards/CardSettings.js
+++ banker_app/client/bank-app/src/components/Cards/CardSettings.js
@@ -117,13 +117,7 @@ export default function CardSettings({ data }) {
                   <input
                     type="text"
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue={
-                      data.address_street +
-                      ", " +
-                      data.address_zipcode +
-                      ", " +
-                      data.address_city
-                    }
+                    defaultValue={data.address_street}
                   />
                 </div>
               </div>
